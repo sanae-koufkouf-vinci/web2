4ccb1ac12c347170ff4cf85779524d357756fba4
koufkouf <sanae.koufkouf@student.vinci.be>
2024-09-27 16:18:01 +0200

    compteur des get

--- exercises/ex 1.2/basic/app.ts
+++ exercises/ex 1.2/basic/app.ts
@@ -6,11 +6,23 @@ import filmRouter from "./routes/films";
 
 const app = express();
 
+let getRequestCount = 0;
+
+app.use((req,res,next) => {
+    if(req.method === 'GET'){
+        getRequestCount++;
+          console.log('Nombre de requêtes GET depuis le démarrage du serveur :${getRequestCount}');
+    }
+    next();
+    
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
 app.use("/films",filmRouter);
+
 
 export default app;
