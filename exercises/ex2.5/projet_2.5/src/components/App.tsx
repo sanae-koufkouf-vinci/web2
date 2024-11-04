462f1c35e5ffcd5ca59ad5d384f713866f798868
koufkouf <sanae.koufkouf@student.vinci.be>
2024-11-04 14:59:36 +0100

    new:ex2.5-events

--- exercises/ex2.5/projet_2.5/src/components/App.tsx
+++ exercises/ex2.5/projet_2.5/src/components/App.tsx
@@ -19,7 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <ClickCounter title="Amazing counter"
-        on10ClickMessage="You are a master in the art of clicking !"/>
+        on10ClickMessage="You are a master in the art of clicking !"
+        onMouseOverMessage="Please click on me now !"/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- exercises/ex2.5/projet_2.5/src/components/ClickCounter.tsx
+++ exercises/ex2.5/projet_2.5/src/components/ClickCounter.tsx
@@ -2,21 +2,29 @@ import { useState } from "react";
 
 interface ClickCounterProps {
     title: string;
-    on10ClickMessage: string; 
+    on10ClickMessage: string;
+    onMouseOverMessage : string; 
     
   }
 
-const ClickCounter = ({title,on10ClickMessage = "Master !",}:ClickCounterProps) => {
+const ClickCounter = ({title,on10ClickMessage,onMouseOverMessage}:ClickCounterProps) => {
 
     const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
     
 
     return(
          <div className="card">
          <p>{title}</p>
-         <button onClick={() => setCount((count) => count + 1)}>
+         
+         <button onClick={() => setCount((count) => count + 1)}
+                 onMouseEnter={() => setIsHovered(true)}
+                 onMouseLeave={() => setIsHovered(false)}
+            >
+           
            count is {count}
          </button>
+         {isHovered ? <p>{onMouseOverMessage}</p> : null}
          <p>
         <p>{count>10 ? on10ClickMessage : ""}</p>
            Edit <code>src/App.tsx</code> and save to test HMR
